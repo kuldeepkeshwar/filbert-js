01a9c2aa32c4f95d5101c477b406f6d68092586f
kuldeepkeshwar <kuldeepkeshwar@gmail.com>
2020-07-04 17:12:07 +0530

    fix redirection

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-const isBrowser = () => ![typeof window, typeof document].includes('undefined');
-if (isBrowser()) {
-  window.location = '/docs/introduction';
-}
-export default () => (
-  <div>
-    <div>loading..</div>
-    <div>redirecting you to https://filbert-js.vercel.app</div>
-  </div>
-);
+
+export default () => {
+  React.useEffect(() => {
+    window.location = '/docs/introduction';
+  }, []);
+  return (
+    <div>
+      <div>loading..</div>
+      <div>redirecting you to https://filbert-js.vercel.app</div>
+    </div>
+  );
+};
