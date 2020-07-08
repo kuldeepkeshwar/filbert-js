3812d6d2b790b89a13babd2a69e7b62bc9f705a2
kuldeepkeshwar <kuldeepkeshwar@gmail.com>
2020-07-08 23:39:43 +0530

    chore: refactor interpolation

--- packages/core/src/keyframes.js
+++ packages/core/src/keyframes.js
@@ -1,11 +1,10 @@
 import { RAW, TYPES, TYPES_KEYFRAMES } from '@filbert-js/types';
 
 import { hash } from './hash';
+import { interpolate } from './interpolate';
 
 export function keyframes(styleTemplates, ...variables) {
-  const styleBlock = styleTemplates.reduce((previous, current, index) => {
-    return `${previous}${variables[index - 1]}${current}`;
-  });
+  const [styleBlock] = interpolate(styleTemplates, variables);
   const keyframesName = hash(styleBlock, TYPES_KEYFRAMES);
   const keyframe = {
     [TYPES]: TYPES_KEYFRAMES,
