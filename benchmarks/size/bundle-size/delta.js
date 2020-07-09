89093e94f71f5f0f7706490ad7d7ac6f5f33a835
kuldeepkeshwar <kuldeepkeshwar@gmail.com>
2020-07-09 16:41:00 +0530

    update benchmark message

--- benchmarks/size/bundle-size/delta.js
+++ benchmarks/size/bundle-size/delta.js
@@ -2,8 +2,16 @@ const fs = require('fs');
 const path = require('path');
 const filesize = require('filesize');
 const chalk = require('chalk');
+const filbertVersion = require('@filbert-js/core/package.json').version;
+const styledVersion = require('styled-components/package.json').version;
+const emotionVersion = require('@emotion/styled/package.json').version;
+
 const baseFramework = 'vanilla-css';
-const frameworks = ['filbert', 'emotion', 'styled-components'];
+const frameworks = [
+  { name: 'filbert', version: filbertVersion },
+  { name: 'emotion', version: emotionVersion },
+  { name: 'styled-components', version: styledVersion },
+];
 const statsFilePath = 'build/build-stats.json';
 
 function readJSON(dir) {
@@ -14,12 +22,32 @@ function readJSON(dir) {
   );
 }
 const baseStats = readJSON(path.join(`../${baseFramework}`));
+chalk.cyan(
+  `Base Application(vanilla CSS)  size after gzip :${filesize(
+    baseStats.total,
+  )}`,
+);
 const deltas = frameworks.map((framework) => {
-  const stats = readJSON(path.join(`../${framework}`));
+  const stats = readJSON(path.join(`../${framework.name}`));
   const delta = stats.total - baseStats.total;
-  return { name: framework, delta, deltaLabel: filesize(delta) };
+  return {
+    ...framework,
+    total: stats.total,
+    delta,
+    deltaLabel: filesize(delta),
+  };
 });
-const message = deltas.reduce((agg, item) => {
-  return `${agg}${chalk.green(item.name)} ${chalk.yellow(item.deltaLabel)}\n`;
-}, chalk.cyan(`Framework sizes after gzip \n`));
+let message = chalk.cyan(`Framework sizes after gzip \n`);
+message = deltas.reduce((previous, item) => {
+  return `${previous}${chalk.green(
+    `${item.name}@${item.version}`,
+  )}   ${chalk.yellow(item.deltaLabel)}\n`;
+}, message);
+const lightest = deltas.reduce((previous, item) => {
+  if (previous.total < item.total) {
+    return previous;
+  }
+  return item;
+});
 console.log(message);
+console.log(chalk.cyan(`Lightest is: ${lightest.name}@${lightest.version}`));
