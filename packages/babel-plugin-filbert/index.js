d77ae94d2ce41c32b1cb3a3107430fe052b71c2e
kuldeepkeshwar <kuldeepkeshwar@gmail.com>
2020-08-06 22:00:41 +0530

    remove pragma dependency

--- packages/babel-plugin-filbert/index.js
+++ packages/babel-plugin-filbert/index.js
@@ -1,3 +1,5 @@
+const babelPluginReactJSX = require('@babel/plugin-transform-react-jsx')
+  .default;
 /**
  * Prepend #__PURE__ comment to help minifiers with
  * dead code elminiation (=DCE)
@@ -10,23 +12,85 @@ function prependPureComment(node) {
     value: '#__PURE__',
   });
 }
-const tags = ['css', 'keyframes'];
-module.exports = function({ types: t }, options = {}) {
-  const name = options.name || 'styled';
+const findLast = (arr, predicate) => {
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (predicate(arr[i])) {
+      return arr[i];
+    }
+  }
+};
+
+function addPragmaImport(path, t, pragmaOptions) {
+  const importDeclaration = t.importDeclaration(
+    [
+      t.importSpecifier(
+        t.identifier(pragmaOptions.import),
+        t.identifier(pragmaOptions.export),
+      ),
+    ],
+    t.stringLiteral(pragmaOptions.module),
+  );
+  const targetPath = findLast(path.get('body'), (p) => p.isImportDeclaration());
+
+  if (targetPath) {
+    targetPath.insertAfter([importDeclaration]);
+  } else {
+    // Apparently it's now safe to do this even if Program begins with directives.
+    path.unshiftContainer('body', importDeclaration);
+  }
+}
+const imports = { styled: 'styled', css: 'css', keyframes: 'keyframes' };
+const pragmaOptions = {
+  import: 'jsx',
+  export: 'jsx',
+  module: '@filbert-js/core',
+};
+function extractPragmaOptions(state) {
+  const [reactjsxPlugin] = state.file.opts.plugins.filter(
+    (p) => p.key === 'transform-react-jsx',
+  );
+  if (reactjsxPlugin && reactjsxPlugin.options) {
+    return { ...reactjsxPlugin.options, pragma: pragmaOptions.import };
+  } else {
+    return { pragma: pragmaOptions.import, pragmaFrag: 'React.Fragment' };
+  }
+}
+module.exports = function (babel, options = {}) {
+  const { types: t } = babel;
+  const _imports = { ...imports, ...options.imports };
   // Enable pure by default if it is not set by the user
   const pure = !('pure' in options) ? true : options.pure;
+  const importSpecifiers = Object.values(_imports);
   return {
     name: 'transform-filbert',
     visitor: {
+      Program: {
+        exit: function (path, state) {
+          if (state.pragmaDetected) {
+            addPragmaImport(path, t, pragmaOptions);
+            babel.traverse(
+              path.parent,
+              babelPluginReactJSX(babel, extractPragmaOptions(state)).visitor,
+              undefined,
+              state,
+            );
+          }
+        },
+      },
+      JSXAttribute: function (path, state) {
+        if (path.node.name.name === 'css') {
+          state.pragmaDetected = true;
+        }
+      },
       TaggedTemplateExpression(path, state) {
         if (
           t.isIdentifier(path.node.tag) &&
-          tags.includes(path.node.tag.name)
+          importSpecifiers.includes(path.node.tag.name)
         ) {
           pure && prependPureComment(path.node);
         } else if (
           t.isIdentifier(path.node.tag.callee) &&
-          path.node.tag.callee.name === name
+          importSpecifiers.includes(path.node.tag.callee.name)
         ) {
           pure && prependPureComment(path.node);
         }
@@ -34,7 +98,11 @@ module.exports = function({ types: t }, options = {}) {
       MemberExpression: {
         exit(path) {
           const node = path.node;
-          if (!t.isIdentifier(node.object) || node.object.name !== name) {
+
+          if (
+            !t.isIdentifier(node.object) ||
+            !importSpecifiers.includes(node.object.name)
+          ) {
             return;
           }
           pure && prependPureComment(path.node);
--- packages/macro/index.js
+++ packages/macro/index.js
@@ -1,42 +1,29 @@
 const { createMacro } = require('babel-plugin-macros');
 const { addNamed } = require('@babel/helper-module-imports');
 
+const babelPlugin = require('babel-plugin-filbert');
+
 const source = '@filbert-js/core';
-const name = 'styled';
 
 function filbertMacro({ references, babel, state }) {
   const program = state.file.path;
 
+  const imports = {};
+
   Object.keys(references).forEach((refName) => {
-    const id = addNamed(program, refName, source);
+    const id = addNamed(program, refName, source, { nameHint: refName });
+
+    imports[refName] = id.name;
     references[refName].forEach((referencePath) => {
       referencePath.node.name = id.name;
     });
   });
 
-  const t = babel.types;
-
-  const namedReferences = references[name] || [];
-
-  namedReferences.forEach((referencePath) => {
-    const path = referencePath.parentPath;
-    const type = path.type;
-    const node = path.node;
-    if (type === 'MemberExpression') {
-      const functionName = node.object.name;
-      let elementName = node.property.name;
-
-      // Support custom elements
-      if (/[A-Z]/.test(elementName)) {
-        elementName = elementName.replace(/[A-Z]/g, '-$&').toLowerCase();
-      }
-
-      path.replaceWith(
-        t.callExpression(t.identifier(functionName), [
-          t.stringLiteral(elementName),
-        ]),
-      );
-    }
-  });
+  babel.traverse(
+    program.parent,
+    babelPlugin(babel, { imports }).visitor,
+    undefined,
+    state,
+  );
 }
 module.exports = createMacro(filbertMacro);
