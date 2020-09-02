48ce8b40dd6816cee9b32074ec953e9cb981717a
anotherjsguy <kuldeepkeshwar@gmail.com>
2020-09-02 11:33:40 +0530

    add twitter icon (#79)
    * add twitter icon
    
    * fix styling for twitter icon

--- website/src/components/Header.jsx
+++ website/src/components/Header.jsx
@@ -1,16 +1,31 @@
 import LogoIcon from './../images/icons/github.svg';
 import React from 'react';
 import { Stack } from 'layout-ui';
+import TwitterIcon from './../images/icons/twitter.svg';
+import { styled } from '@filbert-js/core';
+
+const Link = styled.a`
+  display: inline-flex;
+  align-items: center;
+`;
+
 export function Header() {
   return (
-    <Stack direction="horizontal" align="end">
-      <a
+    <Stack direction="horizontal" align="end" gap="1rem">
+      <Link
         href="https://github.com/kuldeepkeshwar/filbert-js"
         target="_blank"
         rel="noopener noreferrer"
       >
         <LogoIcon />
-      </a>
+      </Link>
+      <Link
+        href="https://twitter.com/filbert_js"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <TwitterIcon />
+      </Link>
     </Stack>
   );
 }
