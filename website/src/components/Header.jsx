be4a4621c36fdfc0eb79afd1fd4fe651b79d5e82
anotherjsguy <kuldeepkeshwar@gmail.com>
2020-09-21 01:43:28 +0530

    added toggle button (#103)
    Co-authored-by: Vivek T A <vivekta20@gmail.com>

--- website/src/components/Header.jsx
+++ website/src/components/Header.jsx
@@ -2,14 +2,18 @@ import LogoIcon from './../images/icons/github.svg';
 import React from 'react';
 import { Stack } from 'layout-ui';
 import TwitterIcon from './../images/icons/twitter.svg';
+import ThemeIcon from './../images/icons/theme.svg';
 import { styled } from '@filbert-js/core';
+import { colors } from './../themes/utils';
 
 const Link = styled.a`
   display: inline-flex;
   align-items: center;
+  color: ${colors(`app.color`)};
 `;
 
-export function Header() {
+export function Header(props) {
+
   return (
     <Stack direction="horizontal" align="end" gap="1rem">
       <Link
@@ -25,6 +29,12 @@ export function Header() {
         rel="noopener noreferrer"
       >
         <TwitterIcon />
+      </Link>
+      <Link
+        onClick={props.toggleTheme}
+        rel="noopener noreferrer"
+      >
+        <ThemeIcon />
       </Link>
     </Stack>
   );
